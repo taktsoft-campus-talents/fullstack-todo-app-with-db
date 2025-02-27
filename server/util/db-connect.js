722364751612258dfbea3c3078a8369248aa588a
Stacy Volvis <strelka1515@gmail.com>
2025-02-27 16:19:57 +0100

    add db to todo app

--- server/util/db-connect.js
+++ server/util/db-connect.js
@@ -4,7 +4,7 @@ const db = knex({
   client: "pg",
   connection: {
     host: process.env.POSTGRES_HOST,
-    // port: process.env.POSTGRES_PORT,
+    port: process.env.POSTGRES_PORT,
     user: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
     database: process.env.POSTGRES_DATABASE,
